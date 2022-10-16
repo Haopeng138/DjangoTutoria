b64450d87632bf46822146064f39bfc079dbb316
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-10-16 16:58:49 +0200

    Add Template

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Members
 from django.template import loader
 # Create your views here.
 
 
 def index(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers':mymembers,
+    }
+    return HttpResponse(template.render(context,request))
